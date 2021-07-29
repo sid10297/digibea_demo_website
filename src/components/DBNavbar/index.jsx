e1f4f2c0a8853d4e3335041e67add092e2d4168f
Siddharth Bhaiji <sid10297@Siddharths-MacBook-Pro.local>
2021-07-29 18:17:04 +0530

    Add Digibea Lable Navbar

--- src/components/DBNavbar/index.jsx
+++ src/components/DBNavbar/index.jsx
@@ -30,63 +30,70 @@ const DBNavbar = () => {
 
   return (
     // <Navbar collapseOnSelect expand={true}>
-    <Navbar
-      collapseOnSelect
-      expand="lg"
-      className={`${showNavbar ? "active" : "hidden"}`}
-    >
-      <Link to="/">
-        <Nav.Link href="#home">
-          <Navbar.Brand>
-            <img className={"navLogo"} src={logo} alt={Strings.appName} />
-          </Navbar.Brand>
-          {/* <NavbarBrand className="brandTitle">
-          Digibea | A Modern Age Digital Marketing Company
-        </NavbarBrand> */}
-        </Nav.Link>
-      </Link>
-
-      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-
-      <Navbar.Collapse id="responsive-navbar-nav">
-        <Nav className="mr-auto">
-          <Link to="/" style={styles.link}>
-            <Nav.Link href="#home">HOME</Nav.Link>
-          </Link>
-
-          <Link to="/about" style={styles.link}>
-            <Nav.Link href="#about">ABOUT</Nav.Link>
-          </Link>
-
-          <NavDropdown title="SERVICES" id="collasible-nav-dropdown">
-            {ServicesData.map((service) => (
-              <Link
-                currentPath="/services"
-                to={`/services/${service.id}`}
-                style={styles.link}
-                key={service.id}
-              >
-                <NavDropdown.Item key={service.id} href={service.id}>
-                  {service.name}
-                </NavDropdown.Item>
-              </Link>
-            ))}
-          </NavDropdown>
-
-          <Link to="/blogs" style={styles.link}>
-            <Nav.Link href="#blogs">BLOGS</Nav.Link>
-          </Link>
-        </Nav>
-
-        <Link to="/getQuote">
-          <Nav.Link href="#getQuote">
-            <Button id="contact-form-button" className="btn">
-              Get Quote
-            </Button>
+    <>
+      <Navbar
+        collapseOnSelect
+        expand="lg"
+        className={`${showNavbar ? "active" : "hidden"}`}
+      >
+        <Link to="/">
+          <Nav.Link href="#home">
+            <Navbar.Brand
+              style={{
+                display: "flex",
+                padding: "0.3rem",
+                marginTop: "0.3rem",
+                color: "var(--brand)",
+              }}
+            >
+              <img className={"navLogo"} src={logo} alt={Strings.appName} />
+              <p style={{ marginLeft: "10px", marginTop: "5px" }}>Digibea</p>
+            </Navbar.Brand>
           </Nav.Link>
         </Link>
-      </Navbar.Collapse>
-    </Navbar>
+
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mr-auto">
+            <Link to="/" style={styles.link}>
+              <Nav.Link href="#home">HOME</Nav.Link>
+            </Link>
+
+            <Link to="/about" style={styles.link}>
+              <Nav.Link href="#about">ABOUT</Nav.Link>
+            </Link>
+
+            <NavDropdown title="SERVICES" id="collasible-nav-dropdown">
+              {ServicesData.map((service) => (
+                <Link
+                  currentPath="/services"
+                  to={`/services/${service.id}`}
+                  style={styles.link}
+                  key={service.id}
+                >
+                  <NavDropdown.Item key={service.id} href={service.id}>
+                    {service.name}
+                  </NavDropdown.Item>
+                </Link>
+              ))}
+            </NavDropdown>
+
+            <Link to="/blogs" style={styles.link}>
+              <Nav.Link href="#blogs">BLOGS</Nav.Link>
+            </Link>
+          </Nav>
+
+          <Link to="/getQuote">
+            <Nav.Link href="#getQuote">
+              <Button id="contact-form-button" className="btn">
+                Get Quote
+              </Button>
+            </Nav.Link>
+          </Link>
+        </Navbar.Collapse>
+      </Navbar>
+    </>
   );
 };
 
